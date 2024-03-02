4ddc78d0dd44db29ac49eef28461d093bccb7437
naflanawas <nafla.20220352@iit.ac.lk>
2024-03-02 22:59:25 +0530

    Initial commit

---
+++ Questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the tallest bulding in the world?",
+    answer: "Burj Khalifa",
+    options: [
+      "Emirates Tower",
+      "Burj Khalifa",
+      "Lotus Tower",
+      "WTC"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which building has the unique feature of 7850 stars that filter sunlight into the museum?",
+    answer: "Louvre Abu Dhabi",
+    options: [
+      "Louvre Paris",
+      "Louvre Abu Dhabi",
+      "Sheikh Zayed Grand Mosque",
+      "Masjidun Noor"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is a planned city that aims to be zero-carbon and zero-waste, and the Dubai Sustainable City, which is a residential community that runs entirely on solar power and has a green central courtyard?",
+    answer: "Masdar City in Abu Dhabi",
+    options: [
+      "Sharjah",
+      "Fujairah",
+      "Masdar City in Abu Dhabi",
+      "Ras Al Khaimah"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is a natural island located off the coast of Abu Dhabi in the United Arab Emirates?",
+    answer: "Sir Bani Yas Island",
+    options: [
+      "Palm Jumeirah",
+      "Bluewaters Island",
+      "Jumeirah Bay Island",
+      "Sir Bani Yas Island"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the man-made lake that sits at its center? The tranquil waters of the lake are surrounded by a pristine sandy beach",
+    answer: "Qudra Oasis",
+    options: [
+      "Qudra Oasis",
+      "Hatta Lake",
+      "Al Mamzar Lake",
+      "Burj Khalifa Lake"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What is the dish cooked with aromatic spices, while the meat is marinated in a blend of spices and then cooked with onions and tomatoes. Vegetables such as carrots, peas, and tomatoes are often added for added flavor and nutrition. It is served with a side of yogurt or a spicy tomato sauce, as well as salad or pickles.",
+    answer: "Machboos",
+    options: [
+      "Khubz",
+      "Machboos",
+      "Chebab",
+      "Mandi"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What features a 10 million-liter tank that houses thousands of marine animals, a 48-meter-long underwater tunnel, a Glass Bottom Boat ride, interactive experiences such as the Shark Encounter and the Underwater Zoo, and state-of-the-art facilities and interactive experiences.",
+    answer: "Dubai Aquarium",
+    options: [
+      "Burj Khalifa",
+      "Ferrari World",
+      "Dubai Aquarium",
+      "Palm Jumeirah"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the most unique and iconic architectural landmarks in dubai?",
+    answer: "Palm Jumeirah",
+    options: [
+      "Dubai Mall",
+      "Palm Jumeirah",
+      "Burj Al Arab",
+      "The Dubai Fountain"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is a popular street food in Dubai and what is it made of?",
+    answer: "Shawarma",
+    options: [
+      "Shawarma",
+      "Burger",
+      "Chebab",
+      "Kebab"
+    ]
+  },{
+    numb: 10,
+    question: "When does dubai food festival take place?",
+    answer: "February or March",
+    options: [
+      "April or May",
+      "September or October",
+      "February or March",
+      "January or February"
+    ]
+  },
+];
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+// ];
